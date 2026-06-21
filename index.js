c6af9675356f48721e2f0a6cb286bea2d1097bd2
Klalofudo Klalofu <klalofudo@gmail.com>
2026-06-21 13:31:21 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+export default {
+  async fetch(request, env) {
+
+    if (request.method !== "POST") {
+      return new Response("WORKS");
+    }
+
+    const update = await request.json();
+
+    const chatId = update.message?.chat?.id;
+
+    if (!chatId) {
+      return new Response("ok");
+    }
+
+    await fetch(`https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: "Жми кнопку Go для старта"
+      })
+    });
+
+    return new Response("ok");
+  }
+}
